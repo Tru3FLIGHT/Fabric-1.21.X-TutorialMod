c3d6647c0e63c7f13e7ebd3e50b9eb21095f6a12
Tru3FLIGHT <Zackarymschaefer@gmail.com>
2025-02-03 23:25:01 -0600

    added alot of things :3

---
+++ src/main/java/com/tru3flight/tutorialmod/item/ModFoodComponents.java
@@ -0,0 +1,11 @@
+package com.tru3flight.tutorialmod.item;
+
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+
+public class ModFoodComponents {
+    public static FoodComponent CAULIFLOWER = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 200), 0.15f).build();
+
+}
