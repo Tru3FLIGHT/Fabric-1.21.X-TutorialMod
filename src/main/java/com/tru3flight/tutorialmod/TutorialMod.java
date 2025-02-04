ba74b83ae88bda53ef5382c44d7f66738f2bbc83
Tru3FLIGHT <Zackarymschaefer@gmail.com>
2025-02-04 14:45:40 -0600

    shoving things up sheeps asses

--- src/main/java/com/tru3flight/tutorialmod/TutorialMod.java
+++ src/main/java/com/tru3flight/tutorialmod/TutorialMod.java
@@ -7,8 +7,15 @@ import com.tru3flight.tutorialmod.item.ModItems;
 import com.tru3flight.tutorialmod.util.HammerUsageEvent;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,5 +33,16 @@ public class TutorialMod implements ModInitializer {
 		ModBlocks.registerModBlocks();
 		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_AHSES, 600);
 		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+		AttackEntityCallback.EVENT.register(((playerEntity, world, hand, entity, entityHitResult) -> {
+			if(entity instanceof SheepEntity sheepEntity && !world.isClient()) {
+				if(playerEntity.getMainHandStack().getItem() == Items.END_ROD) {
+					playerEntity.sendMessage(Text.literal("The player hit a sheep with an end rod, you sick fuck"));
+					playerEntity.getMainHandStack().decrement(1);
+					sheepEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 80, 6));
+				}
+				return ActionResult.PASS;
+			}
+			return ActionResult.PASS;
+		}));
 	}
 }
