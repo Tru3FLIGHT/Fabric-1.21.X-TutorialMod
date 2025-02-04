d5d06d3095dd73e7cee9bccbe6e593dda28e5f5b
Tru3FLIGHT <Zackarymschaefer@gmail.com>
2025-02-03 22:49:05 -0600

    added ChiselItem

--- src/main/java/com/tru3flight/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/tru3flight/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.tru3flight.tutorialmod.block;
 
 import com.tru3flight.tutorialmod.TutorialMod;
+import com.tru3flight.tutorialmod.block.custom.MagicBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
@@ -20,7 +21,6 @@ public class ModBlocks {
             .strength(4f)
             .requiresTool()
             .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create()
             .strength(3f)
@@ -30,12 +30,15 @@ public class ModBlocks {
             new ExperienceDroppingBlock(UniformIntProvider.create(2,5),AbstractBlock.Settings.create()
             .strength(4f)
             .requiresTool()));
-
     public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(3,6),AbstractBlock.Settings.create()
                     .strength(4f)
                     .requiresTool()
                     .sounds(BlockSoundGroup.DEEPSLATE)));
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block", new MagicBlock(AbstractBlock.Settings.create()
+            .strength(1)
+            .requiresTool()));
 
     private static Block registerBlock(String name,Block block) {
         registerBlockItem(name, block);
